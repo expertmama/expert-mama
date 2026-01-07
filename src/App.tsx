e61d02ecad644e55ee1dce87b41c0d9b00ae6b79
Fiad <ultranooba@gmail.com>
2026-01-07 08:18:47 +0600

    Fixed navbar issues

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import Navbar from "./components/Navbar";
 import { colors } from "./components/color";
 function App() {
   return (
-    <><div className="min-h-screen">
+    <>
+      <div className="min-h-screen">
         <Navbar />
-        <div className=" p-6 mx-auto mt-10 max-w-[70%] mx-auto  rounded-b-2xl bg-white/25 backdrop-blur-xl  border border-white/30 shadow-lg">
+        <div className=" p-6 mx-auto mt-10 max-w-[70%] mx-auto rounded-2xl bg-white/25 backdrop-blur-xl  border border-white/30 shadow-lg">
           <h1 style={{ color: colors.blue }} className="text-3xl font-bold">
             Best home service at your doorstep
           </h1>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import logo from "../assets/Avatar.png";
 import userAvatar from "../assets/User-Avatar.png";
-import Colors from "./color";
+import { colors } from './color';
 
 function Navbar() {
   return (
@@ -14,15 +14,16 @@ function Navbar() {
         backdrop-blur-xl
         border border-white/30
         shadow-lg
-        pl-16 pr-16
+        pl-16
+        pr-16
       "
     >
       {/* Brand */}
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-3">
         <img
           src={logo}
           alt="Expert Mama Logo"
-          className="h-12 w-12 object-contain mr-2"
+          className="h-12 w-12 object-contain"
         />
         <div>
           <h1 className="text-3xl font-bold text-slate-900">
@@ -35,13 +36,16 @@ function Navbar() {
       </div>
 
       {/* Actions */}
-      <div className="space-x-6 text-sm flex">
-        <button style={{ backgroundColor: Colors.orange }} className="hover:text-orange-400 p-3 rounded-xl">Dashboard</button>
-        <div className="h-10 w-10 object-contain my-auto rounded-full border border-gray-900">
-          <img
-            src={userAvatar}
-            alt="User Avatar"
-          />
+      <div className="flex items-center space-x-4">
+        <button
+          style={{ backgroundColor: colors.orange }}
+          className="text-white p-3 rounded-xl hover:opacity-90 transition"
+        >
+          Dashboard
+        </button>
+
+        <div className="h-10 w-10 rounded-full border border-gray-900 overflow-hidden">
+          <img src={userAvatar} alt="User Avatar" className="h-full w-full object-cover" />
         </div>
       </div>
     </nav>
