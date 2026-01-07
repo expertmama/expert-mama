df9af69ca1172e0c6773b9ddd662055743989504
Fiad <ultranooba@gmail.com>
2026-01-07 07:09:57 +0600

    Navbar effect added

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,8 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="min-h-screen bg-slate-900 text-white">
+    <div className="min-h-screen text-white">
       <Navbar />
-      <div className="min-h-screen flex flex-col items-center justify-center bg-slate-900 text-white">
-        <h1 className="text-4xl font-bold text-orange-500">
-          Expert Mama
-        </h1>
-        <p className="mt-4 text-lg">
-          সমস্যা আপনার, সমাধান মামার।
-        </p>
-      </div>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import logo from "../assets/Avatar.png";
 
 function Navbar() {
   return (
-    <nav className="flex items-center justify-between px-6 py-4 bg-slate-900 text-white border-b border-slate-700 max-w-[70%] mx-auto">
+    <nav className="flex items-center justify-between px-6 py-4 text-white rounded-b-2xl bg-gray-200 backdrop-blur-3xl backdrop-opacity-50 max-w-[70%] mx-auto">
       {/* Brand */}
       <div className="flex items-center gap-2">
         <img
@@ -11,17 +11,17 @@ function Navbar() {
           className="h-12 w-12 object-contain mr-2"
         />
         <div>
-          <h1 className="text-3xl font-bold text-white">
+          <h1 className="text-3xl font-bold text-slate-900">
             Expert Mama
           </h1>
-          <p className="text-sm text-gray-400">
+          <p className="text-sm text-gray-600">
             সমস্যা আপনার, সমাধান মামার।
           </p>
         </div>
       </div>
 
       {/* Actions */}
-      <div className="space-x-6 text-sm" class="bg-orange-400 p-3 rounded-2xl shadow-lg">
+      <div className="space-x-6 text-sm bg-orange-400 p-3 rounded-xl">
         <button className="hover:text-orange-400">Dashboard</button>
       </div>
     </nav>
